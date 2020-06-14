e6067e3b9e3ecb7e709e479eaa0dfb30b233bdaa
GI Prim <gi4real.gi@gmail.com>
2020-06-14 16:40:15 +0100

    added enviroment variables

--- src/Firebase/Firebase.ts
+++ src/Firebase/Firebase.ts
@@ -1,19 +1,20 @@
-import app from 'firebase/app'
+import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyBP28dbMIUWDXdrn0dR94xOF_E9Ih0Bvlk",
-  authDomain: "fireauth-e58eb.firebaseapp.com",
-  databaseURL: "https://fireauth-e58eb.firebaseio.com",
-  projectId: "fireauth-e58eb",
-  storageBucket: "fireauth-e58eb.appspot.com",
-  messagingSenderId: "325794550944",
-  appId: "1:325794550944:web:db38386c64cd68524aa5c5"
+	apiKey: process.env.REACT_APP_FIREBASE_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 class Firebase {
-  constructor() {
-    app.initializeApp(config);
-  }
+	constructor() {
+		app.initializeApp(config);
+	}
 }
 
-export default Firebase
+export default Firebase;
